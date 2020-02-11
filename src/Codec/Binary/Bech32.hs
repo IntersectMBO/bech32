ceaa1200bd9897c459088a2aac675935afb800f5
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-02-11 08:18:01 +0000

    Improve the Haddock doc outline for module 'Codec.Binary.Bech32'.

--- src/Codec/Binary/Bech32.hs
+++ src/Codec/Binary/Bech32.hs
@@ -6,32 +6,47 @@
 -- (https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki)
 -- address format.
 --
--- From an original implementation by Marko Bencun:
---
--- [sipa/bech32](https://github.com/sipa/bech32/tree/bdc264f84014c234e908d72026b7b780122be11f/ref/haskell)
+-- Based on an [original implementation](https://github.com/sipa/bech32/tree/bdc264f84014c234e908d72026b7b780122be11f/ref/haskell)
+-- by [Marko Bencun](https://github.com/sipa).
 
 module Codec.Binary.Bech32
     (
-      -- * Encoding & Decoding
+    -- * Encoding
       encode
-    , encodeLenient
-    , EncodingError (..)
+
+    -- * Decoding
     , decode
-    , decodeLenient
+
+    -- * Error handling
+    , EncodingError (..)
     , DecodingError (..)
 
-      -- * Data Part
+    -- * Encoding data payloads
     , DataPart
     , dataPartFromBytes
     , dataPartFromText
     , dataPartToBytes
     , dataPartToText
 
-      -- * Human-Readable Part
+    -- * Encoding human-readable prefixes
     , HumanReadablePart
     , HumanReadablePartError (..)
     , humanReadablePartFromText
     , humanReadablePartToText
+
+    -- * Encoding with greater leniency
+    , encodeLenient
+    , decodeLenient
+
+    -- * Constants
+    , dataCharList
+    , encodedStringMaxLength
+    , encodedStringMinLength
+    , humanReadablePartMinLength
+    , humanReadablePartMaxLength
+    , humanReadableCharMinBound
+    , humanReadableCharMaxBound
+    , separatorChar
     ) where
 
 import Codec.Binary.Bech32.Internal
