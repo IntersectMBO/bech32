5d1a2c56d031fd6d2ffca30035aa08e01fc46fd2
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-02-17 09:50:04 +0000

    Add `bech32-th` library with accompanying test suite.
    This package adds Template-Haskell-specific extensions to the `bech32`
    library.
    
    In particular, it includes quasi-quoters capable of checking Bech32
    string literals at compile-time.

---
+++ bech32-th/src/Codec/Binary/Bech32/TH.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- |
+-- Copyright: © 2020 IOHK
+-- License: Apache-2.0
+--
+-- This module contains Template-Haskell-specific extensions to the
+-- [Bech32 library](https://github.com/input-output-hk/bech32).
+
+module Codec.Binary.Bech32.TH
+    (
+      -- ** Quasi-Quotation Support
+      humanReadablePart
+    ) where
+
+import Prelude
+
+import Codec.Binary.Bech32
+    ( HumanReadablePart, humanReadablePartFromText, humanReadablePartToText )
+import Control.Exception
+    ( throw )
+import Data.Text
+    ( Text )
+import Language.Haskell.TH.Quote
+    ( QuasiQuoter (..) )
+import Language.Haskell.TH.Syntax
+    ( Exp, Q )
+
+import qualified Data.Text as T
+
+-- | A quasiquoter for Bech32 human-readable prefixes.
+--
+-- This quasiquoter makes it possible to construct values of type
+-- 'HumanReadablePart' at compile time, using string literals.
+--
+-- Failure to parse a string literal will result in a __compile-time error__.
+--
+-- See 'Codec.Binary.Bech32.HumanReadablePartError' for the set of possible
+-- errors that can be raised.
+--
+-- Example:
+--
+-- >>> :set -XQuasiQuotes
+-- >>> import Codec.Binary.Bech32
+-- >>> import Codec.Binary.Bech32.TH
+-- >>> let addrPrefix = [humanReadablePart|addr|]
+-- >>> addrPrefix
+-- HumanReadablePart "addr"
+-- >>> :t addrPrefix
+-- addrPrefix :: HumanReadablePart
+--
+humanReadablePart :: QuasiQuoter
+humanReadablePart = QuasiQuoter
+    { quoteExp  = quoteHumanReadablePart
+    , quotePat  = notHandled "patterns"
+    , quoteType = notHandled "types"
+    , quoteDec  = notHandled "declarations"
+    }
+  where
+    notHandled things =
+      error $ things <>
+          " are not handled by the Bech32 humanReadablePart quasiquoter."
+
+quoteHumanReadablePart :: String -> Q Exp
+quoteHumanReadablePart = quote
+    . T.unpack
+    . humanReadablePartToText
+    . unsafeHumanReadablePart
+    . T.pack
+  where
+    quote t = [| unsafeHumanReadablePart t |]
+
+unsafeHumanReadablePart :: Text -> HumanReadablePart
+unsafeHumanReadablePart = either throw id . humanReadablePartFromText
---
+++ bech32-th/test/Codec/Binary/Bech32/THSpec.hs
@@ -0,0 +1,92 @@
+module Codec.Binary.Bech32.THSpec
+    ( spec
+    ) where
+
+import Prelude
+
+import Codec.Binary.Bech32
+    ( CharPosition (..)
+    , HumanReadablePartError (..)
+    , humanReadableCharMaxBound
+    , humanReadableCharMinBound
+    , humanReadablePartMaxLength
+    , humanReadablePartMinLength
+    )
+import Codec.Binary.Bech32.TH
+    ( humanReadablePart )
+import Control.Monad
+    ( forM_ )
+import Language.Haskell.TH.Quote
+    ( QuasiQuoter (quoteExp) )
+import Language.Haskell.TH.Syntax
+    ( Exp (..), runQ )
+import Test.Hspec
+    ( Spec, describe, it, shouldSatisfy, shouldThrow )
+
+spec :: Spec
+spec =
+    describe "Quasi-Quotations" $
+
+        describe "Human-Readable Prefixes" $ do
+            let mkHumanReadablePartExp = runQ . quoteExp humanReadablePart
+
+            describe "Parsing valid human-readable prefixes should succeed." $
+                forM_ validHumanReadableParts $ \hrp ->
+                    it (show hrp) $
+                        mkHumanReadablePartExp hrp >>=
+                            (`shouldSatisfy` isAppE)
+
+            describe "Parsing invalid human-readable prefixes should fail." $
+                forM_ invalidHumanReadableParts $ \(hrp, expectedError) ->
+                    it (show hrp) $
+                        mkHumanReadablePartExp hrp
+                            `shouldThrow` (== expectedError)
+
+-- | Matches only function application expressions.
+--
+isAppE :: Exp -> Bool
+isAppE AppE {} = True
+isAppE _       = False
+
+-- | A selection of valid human-readable prefixes, that when parsed with the
+--   'humanReadablePart' quasiquoter should not result in an exception.
+--
+-- Note that this is not by any means intended to be an exhaustive list.
+-- The underlying parsing logic, provided by `humanReadablePartFromText`,
+-- is already tested in the `bech32` package.
+--
+validHumanReadableParts :: [String]
+validHumanReadableParts =
+    [ replicate humanReadablePartMinLength humanReadableCharMinBound
+    , replicate humanReadablePartMaxLength humanReadableCharMaxBound
+    , "addr"
+    ]
+
+-- | A selection of invalid human-readable prefixes, along with the errors that
+--   we expect to see if we attempt to parse them with the 'humanReadablePart'
+--   quasi-quoter.
+--
+-- Note that this is not by any means intended to be an exhaustive list.
+-- The underlying parsing logic, provided by `humanReadablePartFromText`,
+-- is already tested in the `bech32` package.
+--
+invalidHumanReadableParts :: [(String, HumanReadablePartError)]
+invalidHumanReadableParts =
+    [ ( replicate (pred minLen) minChar
+      , HumanReadablePartTooShort
+      )
+    , ( replicate (succ maxLen) maxChar
+      , HumanReadablePartTooLong
+      )
+    , ( replicate (succ minLen) (pred minChar)
+      , HumanReadablePartContainsInvalidChars (CharPosition <$> [0 .. minLen])
+      )
+    , ( replicate (succ minLen) (succ maxChar)
+      , HumanReadablePartContainsInvalidChars (CharPosition <$> [0 .. minLen])
+      )
+    ]
+  where
+    minChar = humanReadableCharMinBound
+    maxChar = humanReadableCharMaxBound
+    minLen = humanReadablePartMinLength
+    maxLen = humanReadablePartMaxLength
