IntersectMBO/bech32

commit ebf77a2e07d3da9b6f14dd7729a8b359b019ca04

fix prettyprinter deprecation

bech32/app/Main.hs
@@ -46,10 +46,10 @@ import Options.Applicative
     , showHelpOnEmpty
     , (<|>)
     )
-import Options.Applicative.Help.Pretty
-    ( bold, hsep, indent, text, underline, vsep )
 import Paths_bech32
     ( version )
+import Prettyprinter
+import Prettyprinter.Render.Terminal
 import System.IO
     ( BufferMode (..), Handle, hSetBuffering, stderr, stdin, stdout )
 
@@ -90,24 +90,24 @@ parse = customExecParser (prefs showHelpOnEmpty) parser
             ]
         , footerDoc $ Just $ vsep
             [ hsep
-                [ text "Supported encoding formats:"
-                , indent 0 $ text  "Base16, Bech32 & Base58."
+                [ pretty "Supported encoding formats:"
+                , indent 0 $ pretty  "Base16, Bech32 & Base58."
                 ]
-            , text ""
-            , text "Examples:"
-            , indent 2 $ hsep [underline $ text "To", text "Bech32:"]
-            , indent 4 $ bold $ text "$ bech32 base16_ <<< 706174617465"
-            , indent 4 $ text "base16_1wpshgct5v5r5mxh0"
-            , text ""
-            , indent 4 $ bold $ text "$ bech32 base58_ <<< Ae2tdPwUPEYy"
-            , indent 4 $ text "base58_1p58rejhd9592uusa8pzj2"
-            , text ""
-            , indent 4 $ bold $ text "$ bech32 new_prefix <<< old_prefix1wpshgcg2s33x3"
-            , indent 4 $ text "new_prefix1wpshgcgeak9mv"
-            , text ""
-            , indent 2 $ hsep [underline $ text "From", text "Bech32:"]
-            , indent 4 $ bold $ text "$ bech32 <<< base16_1wpshgct5v5r5mxh0"
-            , indent 4 $ text "706174617465"
+            , pretty ""
+            , pretty "Examples:"
+            , indent 2 $ hsep [annotate underlined $ pretty "To", pretty "Bech32:"]
+            , indent 4 $ annotate bold $ pretty "$ bech32 base16_ <<< 706174617465"
+            , indent 4 $ pretty "base16_1wpshgct5v5r5mxh0"
+            , pretty ""
+            , indent 4 $ annotate bold $ pretty "$ bech32 base58_ <<< Ae2tdPwUPEYy"
+            , indent 4 $ pretty "base58_1p58rejhd9592uusa8pzj2"
+            , pretty ""
+            , indent 4 $ annotate bold $ pretty "$ bech32 new_prefix <<< old_prefix1wpshgcg2s33x3"
+            , indent 4 $ pretty "new_prefix1wpshgcgeak9mv"
+            , pretty ""
+            , indent 2 $ hsep [annotate underlined $ pretty "From", pretty "Bech32:"]
+            , indent 4 $ annotate bold $ pretty "$ bech32 <<< base16_1wpshgct5v5r5mxh0"
+            , indent 4 $ pretty "706174617465"
             ]
         ]
 
@@ -127,12 +127,12 @@ hrpArgument :: Parser HumanReadablePart
 hrpArgument = argument (eitherReader reader) $ mconcat
     [ metavar "PREFIX"
     , helpDoc $ Just $ vsep
-        [ text "An optional human-readable prefix (e.g. 'addr')."
-        , indent 2 $ text
-            "- When provided, the input text is decoded from various encoding \
+        [ pretty "An optional human-readable prefix (e.g. 'addr')."
+        , indent 2 $ pretty
+            "- When provided, the input pretty is decoded from various encoding \
             \formats and re-encoded to bech32 using the given prefix."
-        , indent 2 $ text
-            "- When omitted, the input text is decoded from bech32 to base16."
+        , indent 2 $ pretty
+            "- When omitted, the input pretty is decoded from bech32 to base16."
         ]
     ]
   where
@@ -183,7 +183,7 @@ detectEncoding str
     | otherwise = resembleBase16 <|> resembleBech32 <|> resembleBase58
   where
     resembleBase16 = do
-        guard (all isHexDigit (toLower <$> str))
+        guard $ all (isHexDigit . toLower) str
         guard (even (length str))
         pure Base16
 

bech32/bech32.cabal
@@ -65,7 +65,9 @@ executable bech32
     , bytestring
     , extra
     , memory
-    , optparse-applicative >= 0.17.0.0 && < 0.18
+    , optparse-applicative
+    , prettyprinter
+    , prettyprinter-ansi-terminal
     , text
   ghc-options:
       -Wall -Wcompat -fwarn-redundant-constraints

bech32/test/AppSpec.hs
@@ -66,7 +66,7 @@ base16 = fromUtf8 . convertToBase Base16 . utf8
 bech32 :: Text -> String -> String
 bech32 txt = T.unpack . Bech32.encodeLenient hrp . dataPartFromBytes . utf8
   where
-    Right hrp = humanReadablePartFromText txt
+    hrp = either (error . ("Error while parsing Bech32: " <>) . show) id $ humanReadablePartFromText txt
 
 
 base58 :: String -> String

bech32/test/Codec/Binary/Bech32Spec.hs
@@ -103,7 +103,7 @@ spec = do
                 -- test that a corrupted checksum fails decoding.
                 let (hrp, rest) =
                         T.breakOnEnd (T.singleton separatorChar) checksum
-                let Just (first, rest') = T.uncons rest
+                let (first, rest') = fromMaybe (error "empty rest") $ T.uncons rest
                 let checksumCorrupted =
                         (hrp `T.snoc` chr (ord first `xor` 1))
                         `T.append` rest'
@@ -188,7 +188,7 @@ spec = do
         it "length > maximum" $ do
             let hrpUnpacked = "ca"
             let hrpLength = length hrpUnpacked
-            let (Right hrp) = humanReadablePartFromText (T.pack hrpUnpacked)
+            let hrp = either (error . ("Error while parsing Bech32: " <>) . show) id $ humanReadablePartFromText (T.pack hrpUnpacked)
             let maxDataLength =
                     Bech32.encodedStringMaxLength
                     - Bech32.checksumLength - Bech32.separatorLength - hrpLength
@@ -198,7 +198,7 @@ spec = do
                 `shouldBe` Left Bech32.EncodedStringTooLong
 
         it "hrp lowercased" $ do
-            let (Right hrp) = humanReadablePartFromText "HRP"
+            let hrp = either (error . ("Error while parsing Bech32: " <>) . show) id $ humanReadablePartFromText "HRP"
             Bech32.encode hrp mempty `shouldBe` Right "hrp1vhqs52"
 
     describe "Arbitrary Bech32String" $
@@ -684,8 +684,10 @@ instance Arbitrary HumanReadablePart where
     arbitrary = do
         len <- choose (1, 10)
         chars <- replicateM len arbitrary
-        let (Right hrp) = humanReadablePartFromText $ T.pack $
-                getHumanReadableChar <$> chars
+        let hrp = either (error . ("Error while parsing Bech32: " <>) . show) id
+                $ humanReadablePartFromText
+                $ T.pack
+                $ getHumanReadableChar <$> chars
         return hrp
     shrink hrp
         | T.null chars = []

cabal.project
@@ -1,3 +1,6 @@
+index-state:
+  , hackage.haskell.org 2023-07-27T22:41:49Z
+
 packages:
   bech32/bech32.cabal
   bech32-th/bech32-th.cabal

flake.nix
@@ -1,0 +1,134 @@
+{
+  description = "bech32";
+
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    iohkNix.url = "github:input-output-hk/iohk-nix";
+    flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
+  };
+
+  outputs = inputs: let
+    supportedSystems = [
+      "x86_64-linux"
+      # disabling to reduce CI time initially. Uncomment later
+      #"x86_64-darwin"
+      #"aarch64-linux"
+      #"aarch64-darwin"
+    ];
+  in
+    inputs.flake-utils.lib.eachSystem supportedSystems (
+      system: let
+        # setup our nixpkgs with the haskell.nix overlays, and the iohk-nix
+        # overlays...
+        nixpkgs = import inputs.nixpkgs {
+          overlays = [
+            inputs.iohkNix.overlays.crypto
+            inputs.haskellNix.overlay
+            inputs.iohkNix.overlays.haskell-nix-crypto
+          ];
+          inherit system;
+          inherit (inputs.haskellNix) config;
+        };
+        inherit (nixpkgs) lib;
+
+        # see flake `variants` below for alternative compilers
+        defaultCompiler = "ghc8107";
+        # We use cabalProject' to ensure we don't build the plan for
+        # all systems.
+        cabalProject = nixpkgs.haskell-nix.cabalProject' ({config, ...}: {
+          src = ./.;
+          name = "bech32";
+          compiler-nix-name = lib.mkDefault defaultCompiler;
+
+          # we also want cross compilation to windows on linux (and only with default compiler).
+          crossPlatforms = p:
+            lib.optional (system == "x86_64-linux" && config.compiler-nix-name == defaultCompiler)
+            p.mingwW64;
+
+          # tools we want in our shell, from hackage
+          shell.tools =
+            {
+              cabal = "3.10.1.0";
+              ghcid = "0.8.8";
+            }
+            // lib.optionalAttrs (config.compiler-nix-name == defaultCompiler) {
+              # tools that work only with default compiler
+              #stylish-haskell = "0.14.4.0";
+              #hlint = "3.5";
+              haskell-language-server = "2.0.0.0";
+            };
+          # and from nixpkgs or other inputs
+          shell.nativeBuildInputs = with nixpkgs; [
+          ];
+          # disable Hoogle until someone request it
+          shell.withHoogle = false;
+          # Skip cross compilers for the shell
+          shell.crossPlatforms = _: [];
+
+          # package customizations as needed. Where cabal.project is not
+          # specific enough, or doesn't allow setting these.
+          modules = [
+            {
+              packages.bech32.configureFlags = ["--ghc-option=-Werror"];
+            }
+          ];
+        });
+        # ... and construct a flake from the cabal project
+        flake = cabalProject.flake (
+          lib.optionalAttrs (system == "x86_64-linux") {
+            # on linux, build/test other supported compilers
+            variants = lib.genAttrs ["ghc8107"] (compiler-nix-name: {
+              inherit compiler-nix-name;
+            });
+          }
+        );
+      in
+        lib.recursiveUpdate flake rec {
+          project = cabalProject;
+          # add a required job, that's basically all hydraJobs.
+          hydraJobs =
+            nixpkgs.callPackages inputs.iohkNix.utils.ciJobsAggregates
+            {
+              ciJobs =
+                flake.hydraJobs
+                // {
+                  # This ensure hydra send a status for the required job (even if no change other than commit hash)
+                  revision = nixpkgs.writeText "revision" (inputs.self.rev or "dirty");
+                };
+            };
+          legacyPackages = rec {
+            inherit cabalProject nixpkgs;
+            # also provide hydraJobs through legacyPackages to allow building without system prefix:
+            inherit hydraJobs;
+            # expose bech32 binary at top-level
+            bech32 = cabalProject.hsPkgs.bech32.components.exes.bech32;
+          };
+          devShells = let
+            profillingShell = p: {
+              # `nix develop .#profiling` (or `.#ghc927.profiling): a shell with profiling enabled
+              profiling = (p.appendModule {modules = [{enableLibraryProfiling = true;}];}).shell;
+            };
+          in
+            profillingShell cabalProject
+            # Additional shells for every GHC version supported by haskell.nix, eg. `nix develop .#ghc927`
+            // lib.mapAttrs (compiler-nix-name: _: let
+              p = cabalProject.appendModule {inherit compiler-nix-name;};
+            in
+              p.shell // (profillingShell p))
+            nixpkgs.haskell-nix.compiler;
+          # formatter used by nix fmt
+          formatter = nixpkgs.alejandra;
+        }
+    );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+  };
+}

commit 9497437ce57a8e0fc6760c7e743be85d9f880de8

Repair broken help strings.
Various help strings were broken in ebf77a2e07d3da9b6f14dd7729a8b359b019ca04,
perhaps by an overly-ambitious search and replace. :D

bech32/app/Main.hs
@@ -129,10 +129,10 @@ hrpArgument = argument (eitherReader reader) $ mconcat
     , helpDoc $ Just $ vsep
         [ pretty "An optional human-readable prefix (e.g. 'addr')."
         , indent 2 $ pretty
-            "- When provided, the input pretty is decoded from various encoding \
+            "- When provided, the input text is decoded from various encoding \
             \formats and re-encoded to bech32 using the given prefix."
         , indent 2 $ pretty
-            "- When omitted, the input pretty is decoded from bech32 to base16."
+            "- When omitted, the input text is decoded from bech32 to base16."
         ]
     ]
   where